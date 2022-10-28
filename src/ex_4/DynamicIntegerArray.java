439d8fcfbf35eba78e059ec35e9769b642ff5c44
piloyaann <armpiloyan53@gmail.com>
2022-10-28 20:57:49 +0400

    commit time

--- src/ex_4/DynamicIntegerArray.java
+++ src/ex_4/DynamicIntegerArray.java
@@ -1,7 +1,45 @@
-public class ex_4 {
+package ex_4;
+
+import java.util.Random;
+
+public class DynamicIntegerArray {
     private int[] array;
 
-    public ex_4(int[] array) {
+    public DynamicIntegerArray(int[] array) {
         this.array = array;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void add(int value){
+        
+    }
+    public void initializeArray(){
+        Random random = new Random();
+        for (int i = 0; i < this.array.length; i++) {
+            this.array[i] = random.nextInt(0, 50);
+        }
+    }
+    public void addAt(int index, int value){
+        int[] arr1 = new int[array.length + 1];
+        for (int i = 0; i < arr1.length; i++) {
+            if (i == index){
+                arr1[i] = value;
+            }
+            else if(i < index) arr1[i] = array[i];
+            else arr1[i] = array[i - 1];
+        }
+        array = arr1;
+    }
+    public void removeAt(int indexOfElement){
+
+    }
+    public void remove(int value){}
+    public void print(){
+        for (int i = 0; i < this.array.length; i++) {
+            System.out.print(this.array[i] + " ");
+        }
     }
 }
--- src/ex_4/Main.java
+++ src/ex_4/Main.java
@@ -1,2 +1,12 @@
-package ex_4;public class Main {
+package ex_4;
+
+public class Main {
+    public static void main(String[] args) {
+        DynamicIntegerArray dynamicArray = new DynamicIntegerArray(new int[6]);
+        dynamicArray.initializeArray();
+        dynamicArray.print();
+        System.out.println();
+        dynamicArray.add(5);
+        dynamicArray.print();
+    }
 }
