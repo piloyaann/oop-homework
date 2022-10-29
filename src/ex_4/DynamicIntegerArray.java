99e9702c13ced0c5a93752bc5121212bd31a2b47
piloyaann <armpiloyan53@gmail.com>
2022-10-29 11:53:07 +0400

    just commit

--- src/ex_4/DynamicIntegerArray.java
+++ src/ex_4/DynamicIntegerArray.java
@@ -4,24 +4,29 @@ import java.util.Random;
 
 public class DynamicIntegerArray {
     private int[] array;
+    int currentSize;
 
     public DynamicIntegerArray(int[] array) {
         this.array = array;
     }
 
-    public int[] getArray() {
-        return array;
-    }
-
-    public void add(int value){
-        
-    }
     public void initializeArray(){
         Random random = new Random();
         for (int i = 0; i < this.array.length; i++) {
             this.array[i] = random.nextInt(0, 50);
         }
     }
+
+    public void add(int value){
+        int[] arr1 = new int[array.length + 1];
+        int k = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = array[i];
+        }
+        array = arr1;
+    }
+
+
     public void addAt(int index, int value){
         int[] arr1 = new int[array.length + 1];
         for (int i = 0; i < arr1.length; i++) {
@@ -31,12 +36,41 @@ public class DynamicIntegerArray {
             else if(i < index) arr1[i] = array[i];
             else arr1[i] = array[i - 1];
         }
+
         array = arr1;
     }
-    public void removeAt(int indexOfElement){
 
+
+    public void removeAt(int indexOfElement){
+        int[] arr1 = new int[this.array.length - 1];
+        int k = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i == indexOfElement){
+                continue;
+            }else {
+                arr1[k++] = array[i];
+            }
+        }
+        array = arr1;
     }
-    public void remove(int value){}
+    public void remove(int value){
+        int countOfSpecificValues = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value){
+                countOfSpecificValues++;
+            }
+        }
+        int[] arr1 = new int[array.length - countOfSpecificValues];
+        int k = 0;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == value){
+                continue;
+            }else {
+                arr1[k++] = array[i];
+            }
+        }
+        array = arr1;
+    }
     public void print(){
         for (int i = 0; i < this.array.length; i++) {
             System.out.print(this.array[i] + " ");
--- src/ex_4/Main.java
+++ src/ex_4/Main.java
@@ -2,11 +2,16 @@ package ex_4;
 
 public class Main {
     public static void main(String[] args) {
-        DynamicIntegerArray dynamicArray = new DynamicIntegerArray(new int[6]);
-        dynamicArray.initializeArray();
-        dynamicArray.print();
-        System.out.println();
-        dynamicArray.add(5);
+        DynamicIntegerArray dynamicArray = new DynamicIntegerArray(new int[4]);
+        dynamicArray.addAt(0, 5);
+        dynamicArray.addAt(1, 5);
+        dynamicArray.addAt(2, 6);
+        dynamicArray.addAt(3, 6);
+        dynamicArray.addAt(4, 6);
+        dynamicArray.addAt(5, 6);
+        dynamicArray.addAt(6, 6);
+        dynamicArray.addAt(7, 6);
+        dynamicArray.addAt(8, 6);
         dynamicArray.print();
     }
 }
